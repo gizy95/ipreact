df13d29ae519407502da0d07bcc6804265a8687f
gizy95 <bjelobrka@gmail.com>
2024-01-15 17:04:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ function App() {
   const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${api_key}`
 
   console.log(api_key);
+  console.log(import.meta.env.BASE_URL);
+  console.log(import.meta.env.MODE);
 
 
   const getData = async () => {
